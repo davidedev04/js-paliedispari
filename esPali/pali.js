4be396e3fed7f7b3e160e366d44e6e9af6447def
Davide Prencipe <davidedev04@gmail.com>
2023-11-28 17:43:22 +0100

    piccola correzione

--- esPali/pali.js
+++ esPali/pali.js
@@ -1,24 +1,25 @@
 
 // chiedere all'utente di inserire una parola
-
-const chiediWord = prompt("inserisci una parola");
-
-console.log(verificaPali(chiediWord));
+let chiediWord = prompt("inserisci una parola");
 
 // verificare se questa parola è palindroma
-function verificaPali(word) {
+function verificaPali() {
 
     // rendere le parole minuscole nel caso
-    const wordLower = word.toLowerCase();
-
+    const wordLower = chiediWord.toLowerCase();
+   
     // invertire la parola
     const reversedWord = wordLower.split('').reverse().join('');
 
-    // verificare se è palindroma
-    if (reversedWord === wordLower) {
+    // verificare se è palindroma, rovesciando la parola
+    if (reversedWord === chiediWord) {
         console.log("Questa parola è palindroma");
     } else {
         console.log("Questa parola non è palindroma");
     }
 
+    return chiediWord;
+
 }
+
+verificaPali();
